d110afe1f37d951495c97b138a2af322ddce32f7
Josef Zacek <joezacek@gmail.com>
2022-02-17 17:53:13 +0000

    create generateQrCode function to generate QR code

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 var qrCodeHolder = document.getElementById("qrcode-holder")
+
+// generate QR code
+function generateQrCode(url){
+  var qrcode = new QRCode(qrCodeHolder, {
+    text: url,
+    width: 200,
+    height: 200,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    correctLevel : QRCode.CorrectLevel.H
+  });
+}
