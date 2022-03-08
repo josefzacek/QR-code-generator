7a8be1aa4e1b48c46165001eef3258cfdbb5a96e
Josef Zacek <joezacek@gmail.com>
2022-03-08 13:33:11 +0000

    declare variables

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let qrCodeHolder = document.getElementById("qrcode-holder")
+let qrCode = document.getElementById("qrcode")
 let generateButton = document.getElementById("generate-qr-button");
 let inputField = document.getElementById("input-url")
 let downloadButton = document.getElementById("download-qr-button");
+let inputFieldWarningTextEmpty = document.getElementById("input-field-warning-text-empty");
+let inputFieldWarningTextEmptySpaces = document.getElementById("input-field-warning-text-empty-space");
 let inputFieldWarningTextUrl = document.getElementById("input-field-warning-text-url");
 let inputFieldWarningTextLength = document.getElementById("input-field-warning-text-length");
+let inputFieldWarningTextDot = document.getElementById("input-field-warning-text-dot");
+let spinner = document.getElementById("spinner");
+let isInputFieldTextFilled = false;
+let hasInputFieldTextEmptySpaces = false;
 let hasInputTextCorrectLength = false;
 let isInputTextCorrectUrl = false;
+let hasInputFieldTextContainDot = false;
+let qrCodeHeight;
 
 // generate QR code
 function generateQrCode(url){
